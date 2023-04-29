b3f6b7495bfe88dd6b5efa230d0abeb1f4319a5b
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-04-29 18:50:28 -0400

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # mcpelauncher-dcblock
+
 This is a mod for mcpelauncher-manifest that blocks double clicking so you don't get banned from servers like zeqa.
+
+
+## Building
+`PATH_TO_NDK="/path/to/ndk"`
+- x86
+
+  `cmake -DCMAKE_TOOLCHAIN_FILE=$PATH_TO_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=x86 ..`
+- x86_64
+
+  `cmake -DCMAKE_TOOLCHAIN_FILE=$PATH_TO_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=x86_64 ..`
+- armeabi-v7a
+
+  `cmake -DCMAKE_TOOLCHAIN_FILE=$PATH_TO_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=armeabi-v7a ..`
+- arm64-v8a
+
+  `cmake -DCMAKE_TOOLCHAIN_FILE=$PATH_TO_NDK/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a ..`
---
+++ src/main.c
@@ -0,0 +1,29 @@
+#include <dlfcn.h>
+#include <sys/time.h>
+long getEpochTime() {
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    return tp.tv_sec * 1000 + tp.tv_usec / 1000;
+}
+char (*Mouse_feed_org)(char, signed char, short, short, short, short);
+long oms = 0;
+void Mouse_feed(char a, signed char b, short c, short d, short e, short f) {
+    if (a >= 1 && a <= 3 && b == 1) {
+        if (getEpochTime() - oms > 50) {
+            Mouse_feed_org(a,b,c,d,e,f);
+            oms = getEpochTime();
+        }
+    } else {
+        Mouse_feed_org(a,b,c,d,e,f);
+    }
+}
+void __attribute__ ((visibility ("default"))) mod_preinit() {
+    void* h = dlopen("libmcpelauncher_mod.so", 0);
+    typedef int (*preinithook)(const char*sym, void*val, void **orig);
+    preinithook mcpelauncher_preinithook;
+    *(void**)(&mcpelauncher_preinithook) = dlsym(h, "mcpelauncher_preinithook");
+    mcpelauncher_preinithook("_ZN5Mouse4feedEcassss", (void*)&Mouse_feed, (void**)&Mouse_feed_org);
+    dlclose(h);
+}
+__attribute__ ((visibility ("default"))) void mod_init() {
+}
