c91c992bc3cbf5ef1e96702908b7dfb15a2c396e
GameParrot <85067619+GameParrot@users.noreply.github.com>
2023-06-17 07:35:36 -0400

    optimize code

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <stdio.h>
+
 bool blockRightDc = false;
 bool logClicks = false;
 bool hasInited = false; // Workaround mcpelauncher mod loading bug
@@ -15,97 +16,90 @@ long getEpochTime() {
     gettimeofday(&tp, NULL);
     return tp.tv_sec * 1000 + tp.tv_usec / 1000;
 }
+
 char (*Mouse_feed_org)(char, signed char, short, short, short, short);
 long oms = 0;
 long oms1 = 0;
+
 void Mouse_feed(char a, signed char b, short c, short d, short e, short f) {
+    long currentEpochTime = getEpochTime();
+
     if (a == 1 && b == 1) {
-        if (getEpochTime() - oms > 50) {
+        if (currentEpochTime - oms > 50) {
             if (logClicks) {
                 __android_log_print(0, "DCBlock", "[■ ] Mouse down");
             }
-            Mouse_feed_org(a,b,c,d,e,f);
-            oms = getEpochTime();
+            Mouse_feed_org(a, b, c, d, e, f);
+            oms = currentEpochTime;
         } else if (logClicks) {
-            __android_log_print(0, "DCBlock", "[■ ] Supressed a DC");
+            __android_log_print(0, "DCBlock", "[■ ] Suppressed a DC");
         }
     } else if (a == 2 && b == 1 && blockRightDc) {
-        if (getEpochTime() - oms1 > 50) {
+        if (currentEpochTime - oms1 > 50) {
             if (logClicks) {
                 __android_log_print(0, "DCBlock", "[ ■] Mouse down");
             }
-            Mouse_feed_org(a,b,c,d,e,f);
-            oms1 = getEpochTime();
+            Mouse_feed_org(a, b, c, d, e, f);
+            oms1 = currentEpochTime;
         } else if (logClicks) {
-            __android_log_print(0, "DCBlock", "[ ■] Supressed a DC");
+            __android_log_print(0, "DCBlock", "[ ■] Suppressed a DC");
         }
     } else {
-        Mouse_feed_org(a,b,c,d,e,f);
+        Mouse_feed_org(a, b, c, d, e, f);
     }
 }
+
 void __attribute__ ((visibility ("default"))) mod_preinit() {
     bool useLegacyMousefeed = false;
+
     if (!hasInited) {
-        FILE * fp;
-        char * line = NULL;
-        size_t len = 0;
-        ssize_t read;
-
-        struct stat buffer;  
-        if (stat("/data/data/com.mojang.minecraftpe/dcblock.conf", &buffer) != 0) {
-            FILE * fp1;
-            fp1 = fopen("/data/data/com.mojang.minecraftpe/dcblock.conf", "w"); // W
-            fputs("BlockRightDc=false\n", fp1);
-            fputs("LogClicks=false\n", fp1);
-            fputs("UseLegacyMousefeed=false\n", fp1);
-            fclose(fp1);
-        }
-
-        fp = fopen("/data/data/com.mojang.minecraftpe/dcblock.conf", "r");
-        while ((read = getline(&line, &len, fp)) != -1) {
-            char *token = strtok(line, "=");
-            if (strcmp(token,"BlockRightDc") == 0) {
-                char* token1 = strtok(NULL, "=");
-                token1[strcspn(token1, "\n")] = 0;
-                if (strcmp(token1,"true") == 0) {
-                    blockRightDc = true;
+        FILE *fp = fopen("/data/data/com.mojang.minecraftpe/dcblock.conf", "r");
+        if (fp) {
+            char* line = NULL;
+            size_t len;
+            ssize_t read;
+            while ((read = getline(&line, &len, fp)) != -1) {
+                char *token = strtok(line, "=");
+                if (token) {
+                    char *value = strtok(NULL, "\n");
+                    if (value) {
+                        if (strcmp(token, "BlockRightDc") == 0) {
+                            blockRightDc = strcmp(value, "true") == 0;
+                        } else if (strcmp(token, "LogClicks") == 0) {
+                            logClicks = strcmp(value, "true") == 0;
+                        } else if (strcmp(token, "UseLegacyMousefeed") == 0) {
+                            useLegacyMousefeed = strcmp(value, "true") == 0;
+                        }
+                    }
                 }
             }
-            if (strcmp(token,"LogClicks") == 0) {
-                char* token1 = strtok(NULL, "=");
-                token1[strcspn(token1, "\n")] = 0;
-                if (strcmp(token1,"true") == 0) {
-                    logClicks = true;
-                }
-            }
-            if (strcmp(token,"UseLegacyMousefeed") == 0) {
-                char* token1 = strtok(NULL, "=");
-                token1[strcspn(token1, "\n")] = 0;
-                if (strcmp(token1,"true") == 0) {
-                    useLegacyMousefeed = true;
-                }
-            }
-        }
-
-        fclose(fp);
-        if (line)
-            free(line);
-
-        __android_log_print(ANDROID_LOG_INFO, "DCBlock", "blockRightDc: %s", blockRightDc ? "true": "false");
-        __android_log_print(ANDROID_LOG_INFO, "DCBlock", "logClicks: %s", logClicks ? "true": "false");
-        __android_log_print(ANDROID_LOG_INFO, "DCBlock", "useLegacyMousefeed: %s", useLegacyMousefeed ? "true": "false");
-        void* h = dlopen("libmcpelauncher_mod.so", 0);
-        typedef int (*preinithook)(const char*sym, void*val, void **orig);
-        preinithook mcpelauncher_preinithook;
-        *(void**)(&mcpelauncher_preinithook) = dlsym(h, "mcpelauncher_preinithook");
-        if (useLegacyMousefeed) {
-            mcpelauncher_preinithook("_ZN5Mouse4feedEccssss", (void*)&Mouse_feed, (void**)&Mouse_feed_org);
+            fclose(fp);
         } else {
-            mcpelauncher_preinithook("_ZN5Mouse4feedEcassss", (void*)&Mouse_feed, (void**)&Mouse_feed_org);
+            // Configuration file not found, create a new one with default values
+            fp = fopen("/data/data/com.mojang.minecraftpe/dcblock.conf", "w");
+            if (fp) {
+                fputs("BlockRightDc=false\n", fp);
+                fputs("LogClicks=false\n", fp);
+                fputs("UseLegacyMousefeed=false\n", fp);
+                fclose(fp);
+            }
         }
-        //dlclose(h);
+
+        __android_log_print(ANDROID_LOG_INFO, "DCBlock", "blockRightDc: %s", blockRightDc ? "true" : "false");
+        __android_log_print(ANDROID_LOG_INFO, "DCBlock", "logClicks: %s", logClicks ? "true" : "false");
+        __android_log_print(ANDROID_LOG_INFO, "DCBlock", "useLegacyMousefeed: %s", useLegacyMousefeed ? "true" : "false");
+
+        void *h = dlopen("libmcpelauncher_mod.so", 0);
+        typedef int (*preinithook)(const char *, void *, void **);
+        preinithook mcpelauncher_preinithook;
+        *(void **)(&mcpelauncher_preinithook) = dlsym(h, "mcpelauncher_preinithook");
+
+        const char *mouseFeedSymbol = useLegacyMousefeed ? "_ZN5Mouse4feedEccssss" : "_ZN5Mouse4feedEcassss";
+        mcpelauncher_preinithook(mouseFeedSymbol, (void *)&Mouse_feed, (void **)&Mouse_feed_org);
+
         hasInited = true;
     }
 }
+
 __attribute__ ((visibility ("default"))) void mod_init() {
 }
